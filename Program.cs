9daeaff3980c8a0501e3a56911c8bb2d7fd18aa1
AlekseiKing <kingulya@mail.ru>
2022-10-05 23:27:25 +0300

    Задача6

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿// Домашнее задание по программе C#
+﻿// Задача №6.
+
+int a;
+
+Console.Write("Введите число а: ");
+int.TryParse(Console.ReadLine()!, out a);
+if (a % 2 == 0)
+{
+    Console.Write($"Да");
+}
+else
+{
+    Console.Write($"Нет");
+}
+
