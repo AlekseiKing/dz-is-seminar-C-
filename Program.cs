25790cd275671b7d127c785e69d73b63e6c35612
AlekseiKing <kingulya@mail.ru>
2022-10-06 00:32:53 +0300

    Задача15

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿// Домашнее задание по программе C#
+﻿// Задача №15.
+
+int day;
+Console.WriteLine("Выводит Да, если выходной. Иначе - нет");
+Console.Write("Введите день недели от 1 до 7: ");
+int.TryParse(Console.ReadLine()!, out day);
+if 
+(day==6 || day ==7) Console.Write("Да");
+else 
+Console.Write("Нет");
+
