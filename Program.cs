0820f4eb41383bffeac7ada1009854fa6e1ed2fb
AlekseiKing <kingulya@mail.ru>
2022-10-05 22:01:02 +0300

    Домашнее задание на C#

--- Program.cs
+++ Program.cs
@@ -1,11 +1,1 @@
-﻿// Задача №2
-
-int a, b;
-Console.Write("Введите число a: ");
-int.TryParse(Console.ReadLine()!, out a);
-Console.Write("Введите число b: ");
-int.TryParse(Console.ReadLine()!, out b);
-if 
-(a > b) Console.WriteLine($"max = {a}, min = {b}");
-else 
-Console.WriteLine($"max = {b}, min = {a}");
+﻿// Домашнее задание по программе C#
