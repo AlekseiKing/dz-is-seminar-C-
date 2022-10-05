f16d555080845abbb5409dfe40865add0caf70c7
AlekseiKing <kingulya@mail.ru>
2022-10-05 22:37:11 +0300

    Zadacha2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿// Домашнее задание по программе C#
+﻿// Задача №2.
+
+int a, b;
+Console.Write("Введите число a: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write("Введите число b: ");
+int.TryParse(Console.ReadLine()!, out b);
+if 
+(a > b) Console.WriteLine($"max = {a}, min = {b}");
+else 
+Console.WriteLine($"max = {b}, min = {a}");
