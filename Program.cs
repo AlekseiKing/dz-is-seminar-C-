0062d47043a98df46efb05deb32a5acc45cfe8bc
AlekseiKing <kingulya@mail.ru>
2022-10-06 00:29:24 +0300

    Задача10

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿// Домашнее задание по программе C#
+﻿// Задача №10.
+
+while (true)
+{
+    int rand = new Random().Next() % 1000;
+    Console.WriteLine(rand);
+    Console.WriteLine($"{(rand%100 - rand%10)/10}");
+    Console.ReadKey();
+}
