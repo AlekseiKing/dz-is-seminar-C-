c4b90b3371d2b3840aa2e1838f5ea638b2ecd7c7
AlekseiKing <kingulya@mail.ru>
2022-10-06 00:49:17 +0300

    Задача13

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// Задача №13.
 
+int a;
+while (true)
+{
+    Console.Write("Введите числ: ");
+    int.TryParse(Console.ReadLine()!, out a);
+    if (a < 100) Console.WriteLine("Третьей цифры нет");
+    else
+    {
+        for(;a>=1000;a/=10);
+        Console.WriteLine($"Третья цифра: {a%10}");
+    }
