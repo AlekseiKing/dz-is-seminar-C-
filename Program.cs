cd473f9bed976f950d2e8af4f50d91d5a59ada05
AlekseiKing <kingulya@mail.ru>
2022-10-05 22:34:41 +0300

    Zadacha4

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿// Домашнее задание по программе C#
+﻿// Задача №4.
+
+int a, b, c, max;
+Console.Write("Введите число a: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write("Введите число b: ");
+int.TryParse(Console.ReadLine()!, out b);
+Console.Write("Введите число c: ");
+int.TryParse(Console.ReadLine()!, out c);
+max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine(max);
