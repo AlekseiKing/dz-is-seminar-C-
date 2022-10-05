6c094f39f6e374fa8b41b687815fb8bd21569bfe
AlekseiKing <kingulya@mail.ru>
2022-10-06 00:38:55 +0300

    Задача13

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ while (true)
 {
     Console.Write("Введите числ: ");
     int.TryParse(Console.ReadLine()!, out a);
-    if (a < 100) Console.WriteLine("Третьей цифры нет");
+    if 
+    (a < 100) Console.WriteLine("Третьей цифры нет");
     else
     {
         for(;a>=1000;a/=10);
