16ae2cc6176b7327e8f01927f604c69affae9ee8
AlekseiKing <kingulya@mail.ru>
2022-10-09 20:51:10 +0300

    zadacha29

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿//Домашнее задание по C#
+﻿// Задача 29. 
+
+/*
+Напишите функцию, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19-> [1, 2, 5, 7, 19]
+6, 1, 33-> [6, 1, 33]
+*/
+
+int[] array;
+array = CreateArray();
+Console.Write($"[");
+for (int i = 0; i < array.Length - 1; i++)
+    Console.Write($"{array[i]}, ");
+Console.Write($"{array[array.Length - 1]}]");
+int[] CreateArray()
+{
+    int[] _array = new int[8];
+    for (int i = 0; i < 8; i++) _array[i] = new Random().Next(0, 1000);
+
+    return _array;
+}
+
